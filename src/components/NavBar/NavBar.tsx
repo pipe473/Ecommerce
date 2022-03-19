6a9dd6eed5b2eae3b795cb690c5cad8a25e671d8
pipe473 <bedoyafelipe743@gmail.com>
2022-03-19 18:45:04 +0100

    Añadiendo fontawesome a mi proyecto

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Nav, Navbar} from 'react-bootstrap';
+import { Nav, Navbar, Container, Badge } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
     return (
             <Navbar bg="dark" variant="dark" expand="lg">
+                <Container>
                     <Link className="navbar-brand" to="/">Ecommerce</Link>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
@@ -12,6 +15,13 @@ const NavBar = () => {
                         <Link className="nav-link" to="/home">Home</Link>
                     </Nav>
                     </Navbar.Collapse>
+                    <FontAwesomeIcon 
+                      icon={faShoppingCart}
+                      color="white" 
+                      size="2x"
+                      style={{cursor: 'pointer'}} />
+                      <Badge bg='secondary'>8</Badge>
+                </Container>
             </Navbar>
     )
 }
