d6f25f2731196a387b97ab00977bb38990d5bb3a
Felipe Bedoya <you@example.com>
2022-03-23 18:57:34 +0100

    Añadiendo estilos y creando componentes para visualizar el carrito

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,11 +1,16 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
+import './styles.scss';
+import Item from './Item/Item';
 
 const Cart = () => {
     const { cartItems } = useContext(CartContext);
     return (
-        <div>
-            { cartItems.length}
+        <div className="shopping-cart">
+            { cartItems.map(item => (
+                <Item key={item.id} item={item} />
+            ))
+            }
         </div>
     )
 }
--- src/components/Cart/Item/Item.tsx
+++ src/components/Cart/Item/Item.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import './styles.scss';
+import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinus, faPlus, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { CartItem } from '../../../types/typeApp';
 
-const Item = () => {
+type Props = {
+    item: CartItem
+}
+
+const Item = ({ item }: Props) => {
     return (
-        <div>
-
-        </div>
+        <>
+            <div className="shopping-cart-item">
+                <div className="item-action">
+                    <Button variant='secondary'>
+                        <FontAwesomeIcon icon={faPlus} color='white' />
+                        <FontAwesomeIcon icon={faMinus} color='white' />
+                    </Button>
+                </div>
+                <div className="item-detail">
+                    <div className="item-detail-image">
+                        <img src={item.image} alt={item.title} />
+                    </div>
+                    <div className="item-detail-info">
+                        <div className="item-detail-info">
+                            {item.title}
+                        </div>
+                        <div className="item-detail-info-prices">
+                            <span>{item.price}</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="item-action-remove">
+                    <Button 
+                        variant='danger'
+                        title='Retir producto'
+                    >
+                    <FontAwesomeIcon icon={faTrashAlt} color='white' />
+                    </Button>
+                </div>
+            </div>
+        </>
     )
 }
 
